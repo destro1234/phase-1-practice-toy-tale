2b979a6a8c41a0833c0e143ad9262f4b9acb1cec
Elias <eliasventura23@gmail.com>
2022-03-28 21:00:49 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  const toyCollection = document.querySelector("#toy-collection")
+
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const form = toyFormContainer.querySelector('form')
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +16,89 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  function renderOneToy(toy) {
+    let div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML = `
+      <h2>${toy.name}</h2>
+      <img src="${toy['image']}" class="toy-avatar" />
+      <p>${toy.likes} likes</p>
+      <button class="like-btn" id="[${toy.id}]">Like ❤️</button>
+    `
+    let likeButton = div.querySelector(".like-btn")
+    likeButton.addEventListener('click', function () {
+      addLike(toy)
+      
+    })
+
+    toyCollection.append(div)
+  }
+
+  function addNewToy(toy) {
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body:JSON.stringify({
+        "name": `${toy.name}`,
+        "image": `${toy.image}`,
+        "likes": 0
+      })
+    }
+    )
+  }
+
+  form.addEventListener('submit', handleSubmit)
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    let toy = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0
+    }
+    addNewToy(toy)
+    renderOneToy((toy))
+  }
+
+
+
+  function addLike(toy) {
+    fetch(`http://localhost:3000/toys/${toy.id}`, {
+      method: 'PATCH',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        "likes": toy.likes+1
+      })
+    })
+    .then(resp => resp.json())
+    .then(function (data) {
+      toy.likes = data.likes
+    })
+  }
+
+  
+
+
+  function getAllToys() {
+    fetch("http://localhost:3000/toys")
+    .then(resp => resp.json())
+    .then(function (toys) {
+      toys.forEach(toy => {
+        renderOneToy(toy);
+        
+      });
+    })
+  }
+
+
+
+
+  getAllToys()
 });
